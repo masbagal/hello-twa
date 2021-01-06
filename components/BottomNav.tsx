176fe67fcaac6b107c007ebbf488fa45035ec04f
masbagal <dimitrij.agal@gmail.com>
2021-01-06 16:33:41 +0700

    assetlink

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,9 +1,14 @@
 import { FiHome, FiList, FiUser, FiBookmark } from 'react-icons/fi';
+import Link from 'next/link';
 
 export default function BottomNav() {
   return (
     <section className='bottom-nav flex flex-row justify-evenly'>
-      <FiHome color='#8f8f8f' size='1.4rem' />
+      <Link href='/' passHref>
+        <a>
+          <FiHome color='#8f8f8f' size='1.4rem' />
+        </a>
+      </Link>
       <FiList color='#8f8f8f' size='1.4rem' />
       <FiBookmark color='#8f8f8f' size='1.4rem' />
       <FiUser color='#8f8f8f' size='1.4rem' />
